68dd01c25b6a53b938fc76a65c9f7a723c368e71
hd-huangchao <chao.huang@advokategroup.com>
2022-08-05 15:22:04 +0800

    add ar

---
+++ Canvas-JS/AR.js-master/three.js/src/new-api/arjs-utils.js
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+
+const Utils = {};
+
+/**
+ * Create a default rendering camera for this trackingBackend. They may be modified later. to fit physical camera parameters
+ *
+ * @param {string} trackingBackend - the tracking to user
+ * @return {THREE.Camera} the created camera
+ */
+Utils.createDefaultCamera = function (trackingMethod) {
+  var trackingBackend =
+    this.parseTrackingMethod(trackingMethod).trackingBackend;
+  // Create a camera
+  if (trackingBackend === "artoolkit") {
+    var camera = new THREE.Camera();
+  } else console.assert(false, "unknown trackingBackend: " + trackingBackend);
+
+  return camera;
+};
+
+/**
+ * parse tracking method
+ *
+ * @param {String} trackingMethod - the tracking method to parse
+ * @return {Object} - various field of the tracking method
+ */
+Utils.parseTrackingMethod = function (trackingMethod) {
+  if (trackingMethod === "best") {
+    trackingMethod = "area-artoolkit";
+  }
+
+  if (trackingMethod.startsWith("area-")) {
+    return {
+      trackingBackend: trackingMethod.replace("area-", ""),
+      markersAreaEnabled: true,
+    };
+  } else {
+    return {
+      trackingBackend: trackingMethod,
+      markersAreaEnabled: false,
+    };
+  }
+};
+
+export default Utils;
