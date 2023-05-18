5f2f071007074efda3f45af26ea2c6353430edde
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-05-18 17:16:52 +0800

    update

--- VUE/vue-vite-chat/vite.config.ts
+++ VUE/vue-vite-chat/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "#": path.resolve(__dirname, "./types"),
+    },
+  },
+});
